fa3423d34a58b7444c483df4546930e36cad4f5b
Joccaren <matthewroutley6@gmail.com>
2016-11-10 22:53:46 +1100

    Add files via upload

---
+++ empires+/common/defines/defines.lua
@@ -0,0 +1,14 @@
+NDefines.NGameplay.TRUCE_YEARS = 5
+NDefines.NGameplay.ETHOS_MAX_POINTS = 4
+NDefines.NGameplay.LEADER_SKILL_CAP = 15
+NDefines.NGameplay.MISSILE_SUBLIGHT_SPEED_MULT = 5
+NDefines.NGameplay.FALLEN_CUSTOM_EMPIRE_SPAWN_CHANCE = 333
+NDefines.NGameplay.DOCKED_FLEET_MAINTENANCE_REDUCTION = 0.75
+NDefines.NSpecies.MAX_ETHIC_POINTS = 4
+NDefines.NSpecies.MAX_TRAIT_POINTS_LEADER = 2
+NDefines.NSpecies.MAX_TRAIT_POINTS_RULER = 3
+NDefines.NArmy.BASE_HEALTH = 300.0
+NDefines.NPop.FOOD_SURPLUS_EXTRA_GROWTH = 0.2
+NDefines.NShip.ENERGY_MAINTENANCE_MUL = 0.012		-- Ship Maintenance Multiplier, Percentage of the cost of the ship, default = 0.004
+NDefines.NShip.MINERAL_MAINTENANCE_MUL = 0.002		-- Default = 0.004
+NDefines.NShip.MILITARY_POWER_EXPONENT = 0.60
